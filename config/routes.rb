0c019efa8bc3b08ed648694356bade62563ce898
n-katou <102382177+n-katou@users.noreply.github.com>
2023-06-23 15:16:22 +0900

    showページまで作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
